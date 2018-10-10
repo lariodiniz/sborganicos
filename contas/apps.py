00afd1c2cbff131a568ce332df08c076db05251d
Lario dos Santos Diniz <lariodiniz@gmail.com>
2018-10-10 19:43:42 -0300

    Corrigido nome para contas

--- contas/apps.py
+++ contas/apps.py
@@ -1,10 +1,5 @@
-
-#coding: utf-8
-__author__ = "Lário dos Santos Diniz"
-
 from django.apps import AppConfig
 
 
-class UsuariosConfig(AppConfig):
-    name = 'usuarios'
-    verbose_name = 'usuarios'
+class ContasConfig(AppConfig):
+    name = 'contas'
